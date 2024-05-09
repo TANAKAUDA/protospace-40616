f379eff7e77eaa77075fb11f7008f39b0cd241e1
TANAKAUDA <hnhkn283@gmail.com>
2024-05-09 20:13:04 +0900

    3

--- db/migrate/20240507070603_rename_occupation_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RenameOccupationColumnToUsers < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :users, :opccupation, :occupation
-  end
-end
--- db/migrate/20240507073642_rename_position_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RenamePositionColumnToUsers < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :users, :positon, :position
-  end
-end
