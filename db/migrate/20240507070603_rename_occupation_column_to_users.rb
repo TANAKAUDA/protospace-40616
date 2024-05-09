6c443cbdbd23a2eb8880008908a94b3aad85fb70
TANAKAUDA <hnhkn283@gmail.com>
2024-05-09 19:33:17 +0900

    commit

--- db/migrate/20240507070603_rename_occupation_column_to_users.rb
+++ db/migrate/20240507070603_rename_occupation_column_to_users.rb
@@ -1,4 +1,4 @@
-class RenameOpccupationColumnToUsers < ActiveRecord::Migration[7.0]
+class RenameOccupationColumnToUsers < ActiveRecord::Migration[7.0]
   def change
     rename_column :users, :opccupation, :occupation
   end
